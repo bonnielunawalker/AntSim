34c352a66e57a5eb78c7c8ad1e591eaadb4e8047
brynwalker <brynwalker1@gmail.com>
2016-09-10 16:41:48 +1000

    ADD: Distribute game logic across classes

---
+++ src/GameLogic.cs
@@ -0,0 +1,29 @@
+﻿using SwinGameSDK;
+using System;
+using System.Collections.Generic;
+
+namespace MyGame
+{
+    public static class GameLogic
+    {
+        private static List<Creature> _creatures = new List<Creature>();
+
+        public static void Process()
+        {
+            SwinGame.ProcessEvents();
+
+            if (GameState.Setup)
+            {
+                _creatures.Add(new Creature(new Location(100, 100)));
+            }
+        }
+
+        public static void DrawObjects()
+        {
+            foreach (Creature c in _creatures)
+            {
+                c.Draw();
+            }
+        }
+    }
+}
---
+++ src/GameState.cs
@@ -0,0 +1,8 @@
+﻿namespace MyGame
+{
+    public static class GameState
+    {
+        public static bool Setup = true;
+        public static bool Exit = false;
+    }
+}
