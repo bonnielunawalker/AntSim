bc2ef00ce923cee21a300b526189f9022f6c1907
brynwalker <brynwalker1@gmail.com>
2016-09-10 16:13:58 +1000

    NEW: Add creature class, inheriting from drawable object

---
+++ src/Creature.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace MyGame
+{
+    public class Creature
+    {
+        private Path _currentPath;
+        private List<Waypoint> _newPath;
+        private Location _location;
+
+        public Creature(Location l)
+        {
+            _location = l;
+        }
+
+        public Path GetPathTo(Location l)
+        {
+           return new Path();
+        }
+    }
+}
---
+++ src/DrawableObject.cs
@@ -0,0 +1,20 @@
+﻿using SwinGameSDK;
+
+namespace MyGame
+{
+    public abstract class DrawableObject : IHasLocation
+    {
+        private Location _location;
+
+        public Location Location
+        {
+            get { return _location; }
+        }
+
+        public void Draw()
+        {
+
+        }
+
+    }
+}
