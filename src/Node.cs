93e1cf7fb5ae21b29523675e7e69bd0ad9c58167
Bryn Walker <brynwalker1@gmail.com>
2016-09-25 19:21:46 +1000

    ADD: Node class and initial A* logic

---
+++ src/Node.cs
@@ -0,0 +1,18 @@
+﻿namespace MyGame
+{
+    public class Node: Location
+    {
+        private readonly int _gScore;
+
+        public Node(int x, int y, int gScore)
+            :base(x, y)
+        {
+            _gScore = gScore;
+        }
+
+        public int GScore
+        {
+            get { return _gScore; }
+        }
+    }
+}
--- src/Path.cs
+++ src/Path.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace MyGame
 {
@@ -8,15 +10,19 @@ namespace MyGame
         private readonly Random _rand;
         private readonly LinkedList<Waypoint> _waypoints;
         private readonly Location _destination;
-        private List<Location> _closed;
-        private List<Location> _open;
+        private List<Node> _closed;
+        private List<Node> _open;
 
         public Path(Location l, Location d)
         {
             _rand = GameLogic.Random;
             _destination = d;
             _waypoints = new LinkedList<Waypoint>();
+            _closed = new List<Node>();
+            _open = new List<Node>();
+            _closed.Add(new Node(l.X, l.Y, 0));
             _waypoints.AddFirst(new Waypoint(new Location(l.X, l.Y), 0));
+            AddNeigbours(_closed.First());
         }
 
         public LinkedList<Waypoint> Waypoints
@@ -57,17 +63,57 @@ namespace MyGame
 
         }
 
-        private void AddNeigbours(Location l)
+        private void AddNeigbours(Node n)
         {
-            Location up = new Location(l.X, l.Y-1);
-            Location down = new Location(l.X, l.Y + 1);
-            Location left = new Location(l.X-1, l.Y);
-            Location right = new Location(l.X+1, l.Y);
+            Node north = new Node(n.X, n.Y - 1, n.GScore);
+            Node south = new Node(n.X, n.Y + 1, n.GScore);
+            Node east = new Node(n.X - 1, n.Y, n.GScore);
+            Node west = new Node(n.X + 1, n.Y, n.GScore);
+            Node northeast = new Node(n.X - 1, n.Y - 1, n.GScore);
+            Node southeast = new Node(n.X - 1, n.Y + 1, n.GScore);
+            Node northwest = new Node(n.X - 1, n.Y + 1, n.GScore);
+            Node southwest = new Node(n.X + 1, n.Y + 1, n.GScore);
 
-            _open.Add(up);
-            _open.Add(down);
-            _open.Add(left);
-            _open.Add(right);
+            _open.Add(north);
+            _open.Add(south);
+            _open.Add(east);
+            _open.Add(west);
+            _open.Add(northeast);
+            _open.Add(southeast);
+            _open.Add(northwest);
+            _open.Add(southwest);
+        }
+
+        private Node GetPriorityNode(Node current)
+        {
+            int score;
+            int lowestScore = 2073600;
+            Node bestNode = null;
+
+            foreach (Location l in _open)
+            {
+                score = GetFScore(current, l);
+
+                if (score < lowestScore)
+                {
+                    lowestScore = score;
+                    bestNode = l as Node;
+                }
+            }
+
+            return bestNode;
+        }
+
+        private int GetFScore(Node current, Location destination)
+        {
+            int manhattanX, manhattanY, distance;
+
+            distance = current.GScore + 1;
+
+            manhattanX = Math.Abs(current.X - destination.X);
+            manhattanY = Math.Abs(current.Y - destination.Y);
+
+            return distance + manhattanX + manhattanY;
         }
     }
 }
